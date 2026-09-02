9a909f031e1134915a7376617fb5a63a74c5848c
osamababeker4-netizen <osamababeker4-netizen@users.noreply.github.com>
2026-09-02 14:13:28 +0300

    Add installable cross-platform PWA

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'asas-lims-pwa-v7-2-1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './runtime-config.js',
+  './manifest.webmanifest',
+  './logo.jpg'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(caches.open(CACHE_NAME).then(function (cache) { return cache.addAll(APP_SHELL); }).then(function () { return self.skipWaiting(); }));
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.filter(function (key) { return key !== CACHE_NAME; }).map(function (key) { return caches.delete(key); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(function (cached) {
+    return cached || fetch(event.request).then(function (response) {
+      if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) { cache.put(event.request, copy); });
+      }
+      return response;
+    }).catch(function () { return caches.match('./index.html'); });
+  }));
+});
