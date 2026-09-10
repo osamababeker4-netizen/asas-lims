fa44646ed863f90eca5cd8bc35589558fc88c84c
osamababeker4-netizen <osamababeker4@gmail.com>
2026-09-11 00:35:26 +0300

    Delete desktop directory

--- desktop/android/settings.gradle.kts
+++
@@ -1,4 +0,0 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
-dependencyResolutionManagement { repositories { google(); mavenCentral() } }
-rootProject.name = "ASASPortal"
-include(":app")
