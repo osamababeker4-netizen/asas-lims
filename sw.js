de19795278e84aa715f46693a06d6734e2b5f6f8
osamababeker4-netizen <osamababeker4@gmail.com>
2026-09-12 15:57:41 +0300

    إصلاح عاجل للصورة وروابط التواصل وإضافة المستخدم

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
-const CACHE_NAME = 'asas-lims-pwa-v8-4-2-clean-login';
-const APP_SHELL = ['./','./index.html','./style.css?v=8-4-2-clean-login','./app-password.js?v=8-4-2-clean-login','./branch-map.js?v=8-1-3-branches','./i18n.js?v=8-4-2-clean-login','./runtime-config.js?v=8-1-1','./manifest.webmanifest','./logo.jpg'];
+const CACHE_NAME = 'asas-lims-pwa-v8-4-3-hotfix';
+const APP_SHELL = ['./','./index.html','./style.css?v=8-4-3-hotfix','./app-password.js?v=8-4-3-hotfix','./branch-map.js?v=8-1-3-branches','./i18n.js?v=8-4-3-hotfix','./runtime-config.js?v=8-1-1','./manifest.webmanifest','./logo.jpg'];
 self.addEventListener('install', function(event) {
   event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(APP_SHELL); }).then(function() { return self.skipWaiting(); }));
 });
