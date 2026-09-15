77490eca05a2172c99943978e3e124f7a9404824
osamababeker4-netizen <osamababeker4@gmail.com>
2026-09-15 15:37:37 +0300

    Update service worker cache after smart upload fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
-const CACHE_NAME = 'asas-lims-pwa-v8-7-0-smart-files';
-const APP_SHELL = ['./','./index.html','./style.css?v=8-7-0-smart-files','./app-password.js?v=8-7-0-smart-files','./branch-map.js?v=8-7-0-smart-files','./i18n.js?v=8-7-0-smart-files','./runtime-config.js?v=8-7-0-smart-files','./manifest.webmanifest','./logo.jpg'];
+const CACHE_NAME = 'asas-lims-pwa-v8-7-1-smart-files-fix';
+const APP_SHELL = ['./','./index.html','./style.css?v=8-7-1-smart-files-fix','./app-password.js?v=8-7-1-smart-files-fix','./branch-map.js?v=8-7-1-smart-files-fix','./i18n.js?v=8-7-1-smart-files-fix','./runtime-config.js?v=8-7-1-smart-files-fix','./manifest.webmanifest','./logo.jpg'];
 self.addEventListener('install', function(event) {
   event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(APP_SHELL); }).then(function() { return self.skipWaiting(); }));
 });
