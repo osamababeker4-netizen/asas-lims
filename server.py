46a7c4f504d14de142ac2e9465a13903a1618514
osamababeker4-netizen <osamababeker4@gmail.com>
2026-09-22 10:21:23 +0000

    Techno V10.8.6: update server release version

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ import urllib.error
 import urllib.request
 
 BASE = os.path.dirname(os.path.abspath(__file__))
-APP_VERSION = '10.8.5-techno-desktop-reset-release'
+APP_VERSION = '10.8.6-responsive-release'
 DB = os.environ.get('LIMS_DB_PATH', os.path.join(BASE, 'lims.db'))
 OFFICIAL_CATALOG = os.path.join(BASE, 'official_test_catalog.json')
 QUALITY_UPLOADS = os.environ.get('LIMS_QUALITY_UPLOADS', os.path.join(BASE, 'uploads', 'quality'))
